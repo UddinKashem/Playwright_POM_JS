c578a6bcc7a7360218908503e2e511e6ae9d2813
UddinKashem <30503392+UddinKashem@users.noreply.github.com>
2026-04-25 17:22:51 -0400

    adding test for A/B testing page

---
+++ pages/A_B_TestingPage.js
@@ -0,0 +1,7 @@
+
+exports.A_B_TestingPage = class A_B_TestingPage {
+    constructor(page) { // Accept 'page' as an argument
+        this.page = page; // Assign it to the class
+        // this.h3_A_B_Test = page.getByRole('heading', { name: 'A/B Test Control' });
+    }
+}
---
+++ tests/demo/a_b_TestingPage.spec.js
@@ -0,0 +1,19 @@
+const { test, expect } = require('@playwright/test');
+const { HomePage } = require('../../pages/home');
+const { A_B_TestingPage } = require('../../pages/A_B_TestingPage');
+
+
+test('Validate A/B Testing Page', async ({ page }) => {
+    const homePage = new HomePage(page);
+    const a_B_TestingPage = new (A_B_TestingPage);
+
+    await homePage.gotoHomePage();
+
+    await expect(page.getByText('Welcome')).toBeVisible();
+    await expect(page.getByRole('heading', { name: 'Available Examples' })).toBeVisible();
+    await expect(homePage.lnk_A_B_Testing).toBeEnabled();
+    await homePage.lnk_A_B_Testing.click();
+    await expect(page).toHaveURL('https://the-internet.herokuapp.com/abtest');
+    await page.pause();
+
+})
